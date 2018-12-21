09ea145967492651c8b6fdb8c17ccf5be29a8d0e
Ramon Chancay <ramon.chancay@gmail.com>
2018-12-21 10:27:42 -0500

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,47 @@
+module.exports = {
+  siteMetadata: {
+    title: `Dariel frases`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@gatsbyjs`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },    
+    'gatsby-plugin-offline',
+    {
+      resolve: `gatsby-source-airtable`,
+      options: {
+        apiKey: process.env.AIRTABLE_API_KEY, // may instead specify via env, see below
+        tables: [
+          {
+            baseId: process.env.AIRTABLE_BASE_ID,
+            tableName: process.env.AIRTABLE_TABLENAME,
+            tableView: process.env.AIRTABLE_TABLEVIEW,
+            
+          },
+        ]
+      }
+    }
+
+  ],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import {graphql, StaticQuery} from 'gatsby';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+const IndexPage = () => (
+  <Layout>
+  <StaticQuery
+    query={graphql`
+      {
+        allAirtable{
+          edges {
+            node {
+              data {
+                nombre
+                frase
+                ejemplo
+              }
+            }
+          }
+        }
+      }
+    `}
+
+    render={data => {
+      
+      const count = data.allAirtable.edges.length -1;
+      const index = Math.round(Math.random() * count);
+      const phrase =  data.allAirtable.edges[index];
+    
+      return (
+        <>
+          <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+          <h1>{phrase.node.data.frase}</h1>
+          <blockquote>{phrase.node.data.ejemplo}</blockquote>
+        </>
+      )
+    }
+  }
+    />
+    </Layout>
+    
+)
+
+export default IndexPage
